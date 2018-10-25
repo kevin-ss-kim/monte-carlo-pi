65dd4cb0f52477da395af6e2feab5fa8024e2b15
Kevin Kim <sskevinkim@gmail.com>
2018-10-25 07:55:04 -0700

    scale circle to git status

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+context.scale(1, 0.5)
 context.beginPath()
 context.arc(0, 0, canvas.width, 0, 2 * Math.PI)
 context.stroke()
